e48664ddb9863d33ed6f32b9da510302dc290cb1
Czarina Asuncion <czarinaasuncion7197@gmail.com>
2026-05-22 22:59:45 +0800

    Delete TheElementarium directory

--- TheElementarium/elements/AlkaliMetal.java
+++
@@ -1,7 +0,0 @@
-package TheElementarium.elements;
-
-public class AlkaliMetal extends Element {
-    public AlkaliMetal(String name, String symbol, int atomicNumber, double atomicWeight, String electronConfig, String applications) {
-        super(name, symbol, atomicNumber, atomicWeight, "Alkali metals", electronConfig, applications);
-    }
-}
--- TheElementarium/elements/PostTransitionMetal.java
+++
@@ -1,7 +0,0 @@
-package TheElementarium.elements;
-
-public class PostTransitionMetal extends Element {
-    public PostTransitionMetal(String name, String symbol, int atomicNumber, double atomicWeight, String electronConfig, String applications) {
-        super(name, symbol, atomicNumber, atomicWeight, "Post-transition metals", electronConfig, applications);
-    }
-}
