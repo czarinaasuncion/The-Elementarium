a1d2411b214c81c78b0d7fcbb9c4855cd74135ed
Czarina Asuncion <czarinaasuncion7197@gmail.com>
2026-05-23 17:13:56 +0800

    Delete TheElementarium directory

--- TheElementarium/elements/ReactiveNonmetal.java
+++
@@ -1,8 +0,0 @@
-package TheElementarium.elements;
-
-public class ReactiveNonmetal extends Element {
-    public ReactiveNonmetal(String name, String symbol, int atomicNumber, double atomicWeight,
-                            String electronConfig, String applications) {
-        super(name, symbol, atomicNumber, atomicWeight, "Reactive nonmetals", electronConfig, applications);
-    }
-}
--- TheElementarium/elements/TransitionMetal.java
+++
@@ -1,7 +0,0 @@
-package TheElementarium.elements;
-
-public class TransitionMetal extends Element {
-    public TransitionMetal(String name, String symbol, int atomicNumber, double atomicWeight, String electronConfig, String applications) {
-        super(name, symbol, atomicNumber, atomicWeight, "Transition metals", electronConfig, applications);
-    }
-}
