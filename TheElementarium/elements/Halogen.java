df3918ab56fb350b2f14f46209aa485f3458d2c5
Czarina Asuncion <czarinaasuncion7197@gmail.com>
2026-05-24 10:36:19 +0800

    Delete TheElementarium directory

--- TheElementarium/elements/Halogen.java
+++
@@ -1,7 +0,0 @@
-package TheElementarium.elements;
-
-public class Halogen extends Element {
-    public Halogen(String name, String symbol, int atomicNumber, double atomicWeight, String electronConfig, String applications) {
-        super(name, symbol, atomicNumber, atomicWeight, "Halogens", electronConfig, applications);
-    }
-}
--- TheElementarium/elements/NobleGas.java
+++
@@ -1,7 +0,0 @@
-package TheElementarium.elements;
-
-public class NobleGas extends Element {
-    public NobleGas(String name, String symbol, int atomicNumber, double atomicWeight, String electronConfig, String applications) {
-        super(name, symbol, atomicNumber, atomicWeight, "Noble gases", electronConfig, applications);
-    }
-}
