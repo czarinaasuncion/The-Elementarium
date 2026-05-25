16fa8ed6fd5bd8adb9ab7223abd1f3862060d55d
Czarina Asuncion <czarinaasuncion7197@gmail.com>
2026-05-25 23:52:41 +0800

    Update DetailPanel.java

--- TheElementarium/elementApp/DetailPanel.java
+++ TheElementarium/elementApp/DetailPanel.java
@@ -28,49 +28,67 @@ public class DetailPanel extends JPanel { //may word na extends to indicate na m
         initComponents();} //call helper method to for UI hierarchy, kumbaga parang sa canva, nakalayer yung graphic elemnts para di lumitaw sa unahan yung dapat nasa likuran
 
     private void initComponents() { //initializations for UI
+        
+        //initialize panel to hold image, and element details
         JPanel topContainer = new JPanel(); 
-        topContainer.setLayout(new BoxLayout(topContainer, BoxLayout.Y_AXIS));
-        topContainer.setOpaque(false);
+        topContainer.setLayout(new BoxLayout(topContainer, BoxLayout.Y_AXIS)); //y-axis para pa vertical/portrait siya
+        topContainer.setOpaque(false); //set to false para di transparent
 
-        lblImage = new JLabel() {
-            @Override
-            protected void paintComponent(Graphics g) {
-                super.paintComponent(g);
-                if (currentGroupImage != null) {
-                    Graphics2D g2d = (Graphics2D) g.create();
-                    g2d.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
-                    g2d.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
-                    g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-                    g2d.drawImage(currentGroupImage, 0, 0, getWidth(), getHeight(), this);
-                    g2d.dispose();}
+        lblImage = new JLabel() { //subclass of JLabel, instead na icon/image lang ihohold niya, may sarili siyang logic
+            @Override //override when the component needs to rendered (for example, if nagresize yung window etc.)
+            protected void paintComponent(Graphics g) { //nag override siya from java.swing.*, specifically java.swing.*
+                // protected since yung mga classes ng package na to gagamit din ng paintComponent()
+                //matik na icacall to
+                super.paintComponent(g); //dito nagpeperform yung Java ng standard clean up (like clearing the bg) bago mag execute/draw yung custom code mo
+                if (currentGroupImage != null) { 
+                    //yung currentGroupImage naghohold siya ng image object
+                    //null means "pointing to nothing", in short, wala pang naka assign na image or yung attempt na iload yung image nag fail
+                    //if statement, bale tinatanong niya na "may image ba talaga to?", 
+                    //!= means not, so ibig sabihin if may naka assign na image, magproproceed siya sa sunod na codes, if null siya edi skip niya yung mga susunod na codes
+                    Graphics2D g2d = (Graphics2D) g.create(); 
+                    //Graphics object na nagproprovide ng advanced control sa rendering features
+                    //yung rendering features yung mga susunod na codes
+
+                    //sinasabi ng setRenderingHint kung pano idradraw ni Java yung image
+                    g2d.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR); //if naka shrink or enlarge yung image, magiiba din yung pixels to adjust para di mukang pixelated ung image
+                    g2d.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY); //para maganda quality ng image
+                    g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON); //ginagamit to para smooth yung edges ng mga round shapes or curves sa image, para walang stair-step effect (aliasing), in other words, para di ule mukang pixelated
+                    g2d.drawImage(currentGroupImage, 0, 0, getWidth(), getHeight(), this); //ginagamit to para ma force yung image na magmatch sa size ng JLabel, para dynamic pa din kahit resize mo window
+                    g2d.dispose();} //memory management, para di magconsume ng sobrang memory yung app, bale di magdradraw ng image if di naman gagamitin
+                //for example, di naman hinover yung ibang element blocks, so yung image nila di naman need. Need lang ng image if naka hover or click
             }
         };
-        lblImage.setAlignmentX(Component.CENTER_ALIGNMENT);
-        lblImage.setBorder(BorderFactory.createLineBorder(Color.GRAY));
+        lblImage.setAlignmentX(Component.CENTER_ALIGNMENT); //para magcenter yung image
+        lblImage.setBorder(BorderFactory.createLineBorder(Color.GRAY)); //para may gray border yung image
 
-        lblName = new JLabel("Name", SwingConstants.CENTER);
-        lblName.setFont(new Font("Arial", Font.BOLD, 22));
-        lblName.setForeground(ElementGUI.DARK_BLUE);
-        lblName.setAlignmentX(Component.CENTER_ALIGNMENT);
+        lblName = new JLabel("Name", SwingConstants.CENTER); //initialize yung yung name label, lilitaw yung "Name"
+        lblName.setFont(new Font("Arial", Font.BOLD, 22)); //ganto font nung "Name"
+        lblName.setForeground(ElementGUI.DARK_BLUE); //eto yung text color (see ElementGUI.java to see color initialization)
+        lblName.setAlignmentX(Component.CENTER_ALIGNMENT); //para naka center yung text
 
+        //same lang den sa lblName, iba lang yung text nakalagay
         lblGroup = new JLabel("Group Type", SwingConstants.CENTER);
         lblGroup.setFont(new Font("Arial", Font.ITALIC, 15));
         lblGroup.setForeground(Color.DARK_GRAY);
         lblGroup.setAlignmentX(Component.CENTER_ALIGNMENT);
 
+        //dito na aadd yung image and labels
+        //nakaindicate din dito yung spacer areas para mas mukang maayos layout
         topContainer.add(lblImage);
         topContainer.add(Box.createRigidArea(new Dimension(0, 8)));
         topContainer.add(lblName);
         topContainer.add(Box.createRigidArea(new Dimension(0, 4)));
         topContainer.add(lblGroup);
 
-        txtDetails = new JTextArea();
-        txtDetails.setEditable(false);
-        txtDetails.setLineWrap(true);
-        txtDetails.setWrapStyleWord(true);
-        txtDetails.setFont(new Font("Arial", Font.PLAIN, 14));
-        txtDetails.setOpaque(false);
+        //nainitialize yung text area
+        txtDetails = new JTextArea(); //yung text area eto yung element details, yung atomic number, e config, atomic mass
+        txtDetails.setEditable(false); //naka false to para di maedit ng user yung text
+        txtDetails.setLineWrap(true); //para hindi isang mahabang line yung text, fofollow ng text yung text area, di siya lalagpas
+        txtDetails.setWrapStyleWord(true); //same lang den sa setLineWrap
+        txtDetails.setFont(new Font("Arial", Font.PLAIN, 14)); //font ng element details
+        txtDetails.setOpaque(false); //para di siya transparent
 
+        //same lang den sa txtDetails, pero element applications naman nakalagay
         txtApp = new JTextArea();
         txtApp.setEditable(false);
         txtApp.setLineWrap(true);
@@ -78,21 +96,27 @@ public class DetailPanel extends JPanel { //may word na extends to indicate na m
         txtApp.setFont(new Font("Arial", Font.PLAIN, 14));
         txtApp.setOpaque(false);
 
+        //para may lalagyan yung texts
         JPanel textContainer = new JPanel();
-        textContainer.setLayout(new BoxLayout(textContainer, BoxLayout.Y_AXIS));
+        textContainer.setLayout(new BoxLayout(textContainer, BoxLayout.Y_AXIS)); //y-axis den para mafollow niya yung detail panel
         textContainer.setOpaque(false);
         textContainer.add(Box.createRigidArea(new Dimension(0, 8)));
-        textContainer.add(txtDetails);
+        textContainer.add(txtDetails); //para lumitaw yung element details
         textContainer.add(Box.createRigidArea(new Dimension(0, 10)));
-        textContainer.add(txtApp);
+        textContainer.add(txtApp); //para lumitaw element applications
 
+        //Adds a scrollbar to the text area if the information is too long to fit
+        //di naman to masyadong nagamit since nakawordwrap naman yung texts, kaya naka _NEVER yung horizontal scrollbar
+        //kaso nga lang ang wordwrap pang horizontal lang so pede lumagpas text vertically
+        //in case lang naman na mas dagdagan yung texts, edi mavieview pa din since may vertical scrollbar (AS_NEEDED)
         JScrollPane scrollPane = new JScrollPane(textContainer, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
-        scrollPane.setOpaque(false);
-        scrollPane.getViewport().setOpaque(false);
-        scrollPane.setBorder(null);
+        scrollPane.setOpaque(false); //transparent sha, pag nagscroll ka di siya lilitaw bes
+        scrollPane.getViewport().setOpaque(false); 
+        scrollPane.setBorder(null); //null, walang border
 
-        add(topContainer, BorderLayout.NORTH);
-        add(scrollPane, BorderLayout.CENTER);}
+        //need to para ma add sa detail panel
+        add(topContainer, BorderLayout.NORTH); //north - sa taas sha ng detail panel
+        add(scrollPane, BorderLayout.CENTER);} //center - gitna ng detail panel
 
     @SuppressWarnings("UseSpecificCatch")
     public void populateData(Element element) {
